aeb78545654100cfe9319f4247dfd787481482b2
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-04-27 15:38:16 +0300

    initial commit

---
+++ app/src/main/java/com/troshchiy/promobar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.troshchiy.promobar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
