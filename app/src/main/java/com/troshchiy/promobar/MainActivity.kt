44782fb0098cb7462044fc7bc37ba70a2443188f
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-04-28 17:58:34 +0300

    wip

--- app/src/main/java/com/troshchiy/promobar/MainActivity.kt
+++ app/src/main/java/com/troshchiy/promobar/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.troshchiy.promobar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Rect
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.troshchiy.promobar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val bounds = Rect()
+
+        binding.description.paint.getTextBounds(
+            "Get 30% off your first order with code",
+            0, "Get 30% off your first order with code".length,
+            bounds
+        )
+
+        val height: Int = bounds.height()
+        val width: Int = bounds.width()
+
+        val promoBannerWidth = binding.promoBanner.width
+
+        val o = 0
     }
 }
