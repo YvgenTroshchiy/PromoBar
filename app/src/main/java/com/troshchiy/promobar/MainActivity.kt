0fa5ffcf16650910c15f75c754959c5a9219e679
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-04-29 15:59:36 +0300

    wip

--- app/src/main/java/com/troshchiy/promobar/MainActivity.kt
+++ app/src/main/java/com/troshchiy/promobar/MainActivity.kt
@@ -2,7 +2,10 @@ package com.troshchiy.promobar
 
 import android.content.Context
 import android.content.res.Resources
-import android.graphics.*
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
 import android.os.Bundle
 import android.text.SpannableStringBuilder
 import android.text.Spanned
